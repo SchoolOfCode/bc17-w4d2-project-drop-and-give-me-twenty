97e59fb08a5a8deca8a8e1b3ffc030d5207857a6
Ryan <ryanshaunsmith@gmail.com>
2024-07-23 12:26:26 +0100

    added comments

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ import helmet from 'helmet';
 const app = express();
 const port = 3000
 
+// Using the helmet middleware package
 app.use(helmet());
 
+// Root level get request handler
 app.get('/', function (req, res) {
     res.send("Hello World");
+    // log request to console
     console.log(req);
 }
 );
 
+// open up listening on port 3000
 app.listen(3000, function () {
     console.log("I am on Port 3000");
 });
