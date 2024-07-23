e7bae81bc344942851ad9b52cb7dc0edb23394b5
Ryan <ryanshaunsmith@gmail.com>
2024-07-23 12:24:49 +0100

    imported helmet package and added middleware layer

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import helmet from 'helmet';
 const app = express();
 const port = 3000
 
-app.get('/',function (req,res){
-    res.send("Hello World")
-    console.log(req)
+app.use(helmet());
+
+app.get('/', function (req, res) {
+    res.send("Hello World");
+    console.log(req);
 }
 );
 
-app.listen(3000, function(){
-console.log("I am on Port 3000")
-})
+app.listen(3000, function () {
+    console.log("I am on Port 3000");
+});
