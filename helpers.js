3848c95f0b581b90aa5c70f2bab7baa1efc546ef
Liban Yusuf <lyus01306@gmail.com>
2024-07-25 10:47:51 +0100

    Error handling of missing input fields

--- helpers.js
+++ helpers.js
@@ -1,6 +1,7 @@
 import crypto from 'crypto';
 
 import { activities } from './activities.js';
+import { error } from 'console';
 
 // helper function to return activities by ID
 export async function getActivitiesById(requestId) {
@@ -13,6 +14,9 @@ export async function getActivitiesById(requestId) {
 
 // add new activity to the activities array
 export async function createNewActivity(newActivity) {
+    if(!newActivity||newActivity.activity_duration === null||newActivity.activity_type === null)
+    {throw new error("You have missed one of the input fields")}
+    else{
     const activity = {
         id : crypto.randomUUID(),
         activity_submitted : Date.now(),
@@ -21,5 +25,5 @@ export async function createNewActivity(newActivity) {
     }
     activities.push(activity);
     console.log(activities);
-    return activity;
+    return activity;}
 };
